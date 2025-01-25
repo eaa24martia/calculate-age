a2a47146f1a6dd4a45fe6b994f98e9137fd7c175
eaa24martia <eaa24martia@gmail.com>
2025-01-25 15:12:38 +0100

    Final commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 "use strict";
 
-let birthyear = prompt("Hvilket år er du født?");
+
+let birthYearInput = prompt("Hvilket år er du født?");
+
+let currentYear = new Date().getFullYear();
+
+let birthYear = parseInt(birthYearInput, 10);
+
+let age = currentYear - birthYear;
+
+console.log(`Du er ${age} år gammel!`);
